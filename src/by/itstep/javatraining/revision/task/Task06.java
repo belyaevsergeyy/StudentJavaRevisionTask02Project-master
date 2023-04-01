a1d361176800d15fea75a41efa3c1d2ccbbb169c
SergeyBelyaev <belyaevsergeyy@rambler.ru>
2023-04-01 16:14:57 +0300

    Task 06 well done

--- src/by/itstep/javatraining/revision/task/Task06.java
+++ src/by/itstep/javatraining/revision/task/Task06.java
@@ -23,6 +23,9 @@ package by.itstep.javatraining.revision.task;
 
 public class Task06 {
     public static boolean task06(int a, int b, int c) {
+        if (a > 0 && b > 0 && c > 0) {
+            return (a + b > c) && (a + c > b) && (b + c > a);
+        }
         return false;
     }
 }
